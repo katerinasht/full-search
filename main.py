4862496c583667df9097d2149dfbaaf8f910001c
Smart developer <developer@yandex.ru>
2023-02-13 19:34:07 +0300

    Большая задача по Maps API. Часть №2 v1.0

--- main.py
+++ main.py
@@ -3,6 +3,46 @@ from get_spn import get_spn
 import pygame
 import requests
 
+def pygame_visual():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    spn = get_spn(corners)
+    show_map(",".join(spn), screen)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                try:
+                    if event.key == pygame.K_PAGEUP:
+                        spn[0] = str(float(spn[0]) + 0.05)
+                        spn[1] = str(float(spn[1]) + 0.05)
+                        show_map(",".join(spn), screen)
+                    if event.key == pygame.K_PAGEDOWN:
+                        spn[0] = str(float(spn[0]) - 0.05)
+                        spn[1] = str(float(spn[1]) - 0.05)
+                        show_map(",".join(spn), screen)
+                except Exception:
+                    print("Упс! Что-то пошло не так")
+    pygame.quit()
+
+
+def show_map(spn, screen):
+    geocoder_api_server = "http://static-maps.yandex.ru/1.x/?"
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": spn,
+        "l": "map"
+    }
+    response = requests.get(geocoder_api_server, params=map_params)
+    with tempfile.NamedTemporaryFile() as fp:
+        fp.write(response.content)
+        fp.seek(0)
+        screen.blit(pygame.image.load(fp), (0, 0))
+        pygame.display.flip()
+
+
 toponym_to_find = ",".join(input().split())
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
@@ -11,26 +51,12 @@ geocoder_params = {
     "format": "json"}
 response = requests.get(geocoder_api_server, params=geocoder_params)
 if not response:
-    pass
-json_response = response.json()
-corners = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["boundedBy"]["Envelope"]
-toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-toponym_coodrinates = toponym["Point"]["pos"]
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-geocoder_api_server = "http://static-maps.yandex.ru/1.x/?"
-map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join(get_spn(corners)),
-    "l": "map"
-}
-response = requests.get(geocoder_api_server, params=map_params)
-with tempfile.NamedTemporaryFile() as fp:
-    fp.write(response.content)
-    pygame.init()
-    fp.seek(0)
-    screen = pygame.display.set_mode((600, 450))
-    screen.blit(pygame.image.load(fp), (0, 0))
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+    print("Упс! Что-то пошло не так")
+else:
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    corners = toponym["boundedBy"]["Envelope"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    pygame_visual()
+
