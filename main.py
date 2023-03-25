a585061bc1992274aa9998606c3131b1e4480c39
Your Name <you@example.com>
2023-03-25 20:15:48 +0300

    Большая задача по Maps API. Часть №5

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import requests
 
 def pygame_visual(toponym_longitude, toponym_lattitude):
     pygame.init()
-    screen = pygame.display.set_mode((600, 450))
+    screen = pygame.display.set_mode((600, 500))
     spn = get_spn(corners)
+    points = []
+    text = ""
     l = "map"
-    show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+    show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
     running = True
     while running:
         for event in pygame.event.get():
@@ -19,51 +21,89 @@ def pygame_visual(toponym_longitude, toponym_lattitude):
                     if event.key == pygame.K_PAGEUP:
                         spn[0] = str(float(spn[0]) + 0.05)
                         spn[1] = str(float(spn[1]) + 0.05)
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.key == pygame.K_PAGEDOWN:
                         spn[0] = str(float(spn[0]) - 0.05)
                         spn[1] = str(float(spn[1]) - 0.05)
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.key == pygame.K_UP:
                         toponym_lattitude = str(float(toponym_lattitude) + 0.05)
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.key == pygame.K_DOWN:
                         toponym_lattitude = str(float(toponym_lattitude) - 0.05)
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.key == pygame.K_RIGHT:
                         toponym_longitude = str(float(toponym_longitude) + 0.05)
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.key == pygame.K_LEFT:
                         toponym_longitude = str(float(toponym_longitude) - 0.05)
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
+                    if len(pygame.key.name(event.key)) == 1:
+                        text += to_rus(pygame.key.name(event.key))
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
+                    elif pygame.key.name(event.key) == "space":
+                        text += " "
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
+                    elif pygame.key.name(event.key) == "backspace" and text != "":
+                        text = text[:-1]
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                 except Exception:
                     print("Упс! Что-то пошло не так")
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.pos[0] > 10 and event.pos[0] < 75:
                     if event.pos[1] > 10 and event.pos[1] < 25:
                         l = "map"
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.pos[1] > 30 and event.pos[1] < 45:
                         l = "sat"
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
                     elif event.pos[1] > 50 and event.pos[1] < 65:
                         l = "sat,skl"
-                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l)
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
+                if event.pos[0] > 530 and event.pos[0] < 595 and event.pos[1] > 465 and event.pos[1] < 485:
+                        toponym_longitude, toponym_lattitude = new_place(text)
+                        points.append(",".join([toponym_longitude, toponym_lattitude, "pmlbs"]))
+                        text = ""
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
     pygame.quit()
 
 
-def show_map(spn, screen, toponym_longitude, toponym_lattitude, l):
+def to_rus(text):
+    layout = dict(zip(map(ord, "qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
+                               'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'),
+                      "йцукенгшщзхъфывапролджэячсмитьбю.ё"
+                      'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'))
+    return text.translate(layout)
+
+def new_place(text):
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x/?"
+    response = requests.get(geocoder_request, params={"apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                                                      "geocode": text, "format": "json"})
+    if response.ok:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+        return str(toponym.split()[0]), str(toponym.split()[1])
+
+def show_map(spn, screen, toponym_longitude, toponym_lattitude, l, text, points):
     geocoder_api_server = "http://static-maps.yandex.ru/1.x/?"
     map_params = {
         "ll": ",".join([toponym_longitude, toponym_lattitude]),
         "spn": spn,
-        "l": l
+        "l": l,
+        "pt": "~".join(points)
     }
     response = requests.get(geocoder_api_server, params=map_params)
     with tempfile.NamedTemporaryFile() as fp:
         fp.write(response.content)
         fp.seek(0)
         screen.blit(pygame.image.load(fp), (0, 0))
+        pygame.draw.rect(screen, (0, 0, 0), (0, 450, 600, 50))
+        pygame.draw.rect(screen, (255, 255, 255), (530, 465, 65, 20))
+        font = pygame.font.SysFont(None, 27)
+        img = font.render('Искать', True, (0, 0, 0))
+        screen.blit(img, (530, 465))
+        img = font.render(text, True, (255, 255, 255))
+        screen.blit(img, (10, 465))
         pygame.draw.rect(screen, (255, 255, 255), (10, 10, 65, 15))
         font = pygame.font.SysFont(None, 24)
         img = font.render('схема', True, (0, 0, 0))
@@ -79,7 +119,8 @@ def show_map(spn, screen, toponym_longitude, toponym_lattitude, l):
         pygame.display.flip()
 
 
-toponym_to_find = ",".join(input().split())
+#toponym_to_find = ",".join(input().split())
+toponym_to_find = "33,44"
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
