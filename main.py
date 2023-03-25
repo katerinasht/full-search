73f55a221f6d9a500e10b662f263c1656e561d02
Your Name <you@example.com>
2023-03-25 20:49:38 +0300

    Большая задача по Maps API. Часть №7

--- main.py
+++ main.py
@@ -60,10 +60,14 @@ def pygame_visual(toponym_longitude, toponym_lattitude):
                     elif event.pos[1] > 50 and event.pos[1] < 65:
                         l = "sat,skl"
                         show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
-                if event.pos[0] > 530 and event.pos[0] < 595 and event.pos[1] > 465 and event.pos[1] < 485:
+                if event.pos[0] > 530 and event.pos[0] < 595:
+                    if event.pos[1] > 465 and event.pos[1] < 485:
                         toponym_longitude, toponym_lattitude = new_place(text)
                         points.append(",".join([toponym_longitude, toponym_lattitude, "pmlbs"]))
                         text = ""
+                        show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
+                    if event.pos[1] > 435 and event.pos[1] < 455 and points != []:
+                        points = points[:-1]
                         show_map(",".join(spn), screen, toponym_longitude, toponym_lattitude, l, text, points)
     pygame.quit()
 
@@ -102,6 +106,10 @@ def show_map(spn, screen, toponym_longitude, toponym_lattitude, l, text, points)
         font = pygame.font.SysFont(None, 27)
         img = font.render('Искать', True, (0, 0, 0))
         screen.blit(img, (530, 465))
+        pygame.draw.rect(screen, (255, 255, 255), (530, 435, 65, 20))
+        font = pygame.font.SysFont(None, 27)
+        img = font.render('Отмена', True, (0, 0, 0))
+        screen.blit(img, (530, 435))
         img = font.render(text, True, (255, 255, 255))
         screen.blit(img, (10, 465))
         pygame.draw.rect(screen, (255, 255, 255), (10, 10, 65, 15))
@@ -119,7 +127,7 @@ def show_map(spn, screen, toponym_longitude, toponym_lattitude, l, text, points)
         pygame.display.flip()
 
 
-toponym_to_find = ",".join(input().split())
+toponym_to_find = "33,44"  # ",".join(input().split())
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
